b8d97696e16788af89305d0f96c5b96adc6f0a37
Rodrigo <rodrigo.valentim.si15@gmail.com>
2018-03-06 14:03:19 -0300

    incial

---
+++ src/main/java/br/ufc/npi/controller/TimeController.java
@@ -0,0 +1,15 @@
+package br.ufc.npi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping(path="/times/")
+public class TimeController {
+	@RequestMapping(path="/")
+	public String index (){
+		return "times";
+	}
+
+}
